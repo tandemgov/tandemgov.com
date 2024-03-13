abe1a7ba9b0f1e79c52ec5a99f1d999c3caf3452
infinityplusone <jonathan@infinityplusone.com>
2024-03-12 20:49:06 -0400

    social icons

--- _data/tandemgov.js
+++ _data/tandemgov.js
@@ -1,7 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = function() {
   return {
-    HTTP_HOST: process.env.HTTP_HOST || "https://tandemgov.com"
+    HTTP_HOST: process.env.HTTP_HOST || "https://tandemgov.com",
+    social: [{
+        name: "LinkedIn",
+        slug: "linkedin",
+        url: "https://www.linkedin.com/company/tandemgov",
+    }, {
+        name: "GitHub",
+        slug: "github",
+        url: "https://github.com/tandemgov",
+    }]
   };
 };
